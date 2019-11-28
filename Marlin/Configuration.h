b368a350530f96c45e3f81fa8610c01414003a3d
Marlon Tojal <marlont@backbase.com>
2019-11-28 15:06:02 +0100

    fix max extrude lenght

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -545,7 +545,7 @@
  * Note: For Bowden Extruders make this large enough to allow load/unload.
  */
 #define PREVENT_LENGTHY_EXTRUDE
-#define EXTRUDE_MAXLENGTH 600
+#define EXTRUDE_MAXLENGTH 200
 
 //===========================================================================
 //======================== Thermal Runaway Protection =======================
